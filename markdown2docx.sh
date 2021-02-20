0cc8af2db851284c0bf930a708b5187290c6c199
Gregory M. Dulin <gregory.dulin@gmail.com>
2021-02-19 23:33:26 -0500

    Convert any file ending in resume.md
    Add custom-reference.docx for format modifications

--- markdown2docx.sh
+++ markdown2docx.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-#pandoc "$1" -o ${1%.*}.docx
-pandoc resume.md -o resume.docx
+# https://stackoverflow.com/a/246128/2895343
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+while read path; do
+  pandoc "${path}" -o "${path%.*}.docx" --reference-doc="${DIR}/custom-reference.docx"
+done < <(find "${DIR}" -name '*resume.md')
